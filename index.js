e2e8b7193a6b3ffc6a4fbf5783f281ecc4535bcf
jworkman <jworkman@fullsail.com>
2023-01-09 10:27:19 -0500

    Adding new documentation to my index.js file. References #1

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const spotify = require('./middlewares/spotify')
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cors())
 
+// This is a comment that closes a ticket
 app.use('/spotify/v1', spotify)
 
 app.listen(3001)
