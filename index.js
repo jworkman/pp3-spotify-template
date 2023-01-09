56b0618fad53f466cde4e9d37b2d913e553ddef6
jworkman <jworkman@fullsail.com>
2023-01-09 10:29:21 -0500

    Added a new comment line that closes #1

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const spotify = require('./middlewares/spotify')
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cors())
 
-// This is a comment that closes a ticket
+// This is a comment that references a ticket
+// This is a new comment that closes a ticket
 app.use('/spotify/v1', spotify)
 
 app.listen(3001)
